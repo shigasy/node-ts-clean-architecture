6f2f6ec36f6170a9d322cad5cce606001d1cfc29
shigasy <kxx555u.u@gmail.com>
2021-07-31 06:43:21 +0900

    user entity を追加

---
+++ src/entity/User.ts
@@ -0,0 +1,27 @@
+class User {
+  private _id: number;
+  private _name: string;
+  private _age: number;
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this.name = name;
+  }
+
+  constructor(id: number, name: string, age: number) {
+    this._id = id;
+    this._name = name;
+    this._age = age;
+  }
+}
+
+const UserBusinessRule = {
+  isNameLengthValid(name: string): boolean {
+    return name.length > 0;
+  },
+};
+
+const a = new UserBusinessRule();
